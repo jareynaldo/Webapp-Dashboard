82d6141899a38eda4f0d094343050c1293d5a92a
José Reynaldo <ja.reynaldo@outlook.com>
2022-03-30 21:55:46 -0400

    night 5

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const alertBanner = document.getElementById("alert");
 
     alertBanner.innerHTML =
     `<div class="alert-banner">
-    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+    <p class="alrt-first"><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
     to complete</p>
     <p class="alert-banner-close">x</p>
     </div>
